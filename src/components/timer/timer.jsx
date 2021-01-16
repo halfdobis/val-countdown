1b560f14a5a645844fc410d00162abaa8438fad3
missdobis <missdobis@gmail.com>
2021-01-16 16:43:10 +0100

    countdown

---
+++ src/components/timer/timer.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+import './timer.css';
+
+
+const Timer = () => {
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const CountdownDate = new Date('Feb 14, 2021 00:00:00').getTime();
+
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = CountdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        //stop the timer
+        clearInterval(interval.current);
+      } else {
+        //update the timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  //componentDidMount
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
+  return (
+    <section className="timer-container">
+      <section className="timer">
+        <div>
+          <span className="fa fa-clock timer-icon"></span>
+          <h1>Countdown Timer</h1>
+          <p className="timer-text">Countdown to valinetine!!</p>
+        </div>
+        <div>
+          <section>
+            <p>{timerDays}</p>
+            <p><small>Days</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <p><small>Hours</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <p><small>Minutes</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <p><small>Seconds</small></p>
+          </section>
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default Timer;
